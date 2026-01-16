0bd68a0a879e196a7c912c6f67cad96e7c8f640d
Aman Prasad <amanprasad.9576@gmail.com>
2026-01-16 11:20:35 +0530

    correction in router

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { mongoose } = require('mongoose');
+
+const { serverConfig } = require('./config/index.js');
+const apiRouter = require('./routers/apiRouter.js');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api',apiRouter);
+
+app.get('/', (req, res) => {
+    res.send("Project Starts");
+});
+
+app.listen(serverConfig.PORT, async() => {
+    console.log(`Server is running on port ${serverConfig.PORT}`);
+    try {
+        await mongoose.connect(serverConfig.DEV_DB_URL);
+        console.log("Connected to the database");
+    } catch (err) {
+        console.error("Database connection error:", err);
+    }
+});
+
+
+
+/*
+https://localhost/api/v1/movie 
+ api --> v1 --> movie 
+
+*/
+
